823c95607a6d8c82d9381994c259d75b6e7613d7
Jorgji Sinani <jorgjisinani@gmail.com>
2022-07-17 21:13:41 +0200

    third commit

--- src/first_git_package/MyGitClass.java
+++ src/first_git_package/MyGitClass.java
@@ -3,5 +3,7 @@ package first_git_package;
 public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("I am learning GitHub!!");
+        System.out.println("I am adding a new code");
+        System.out.println("I am adding one more");
     }
 }
