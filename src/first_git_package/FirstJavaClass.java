afbad53f083c7faa5aed6955df0f4cac779dd95d
Jorgji Sinani <jorgjisinani@gmail.com>
2022-07-19 23:13:50 +0200

    first class conflict

--- src/first_git_package/FirstJavaClass.java
+++ src/first_git_package/FirstJavaClass.java
@@ -1,9 +1,7 @@
 package first_git_package;
 
 public class FirstJavaClass {
-<<<<<<< HEAD
+
     //This is my first class
-=======
-//This is form remote repository
->>>>>>> b418445cc06af16ff3d737a30dba04743669c236
+
 }
